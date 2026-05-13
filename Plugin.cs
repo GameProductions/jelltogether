772478518ae2b58a35efb9accd6621aa4bf97950
morenicano <831438+morenicano@users.noreply.github.com>
2026-05-12 22:13:47 -0400

    [v1.1.3] Fix Jellyfin menu resources

--- Plugin.cs
+++ Plugin.cs
@@ -10,6 +10,8 @@ namespace JellTogether.Plugin
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
+        private const string WebResourcePrefix = "JellTogether.Web";
+
         public override string Name => "JellTogether";
         public override Guid Id => Guid.Parse("f9e1e2d3-a4b5-4c6d-8e9f-0a1b2c3d4e5f");
         public override string Description => "Premium Watch Party and Chat for Jellyfin.";
@@ -32,7 +34,7 @@ namespace JellTogether.Plugin
                 {
                     Name = "jelltogether",
                     DisplayName = "JellTogether",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.jelltogether.html",
+                    EmbeddedResourcePath = WebResourcePrefix + ".jelltogether.html",
                     EnableInMainMenu = true,
                     MenuSection = "server",
                     MenuIcon = "movie"
@@ -41,7 +43,7 @@ namespace JellTogether.Plugin
                 {
                     Name = "jelltogether-companion",
                     DisplayName = "JellTogether",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.jelltogether.html",
+                    EmbeddedResourcePath = WebResourcePrefix + ".jelltogether.html",
                     EnableInMainMenu = true,
                     MenuSection = "home",
                     MenuIcon = "movie"
@@ -49,17 +51,17 @@ namespace JellTogether.Plugin
                 new PluginPageInfo
                 {
                     Name = "jelltogether.css",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.jelltogether.css"
+                    EmbeddedResourcePath = WebResourcePrefix + ".jelltogether.css"
                 },
                 new PluginPageInfo
                 {
                     Name = "jelltogether.js",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.jelltogether.js"
+                    EmbeddedResourcePath = WebResourcePrefix + ".jelltogether.js"
                 },
                 new PluginPageInfo
                 {
                     Name = "strings.js",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.strings.js"
+                    EmbeddedResourcePath = WebResourcePrefix + ".strings.js"
                 },
                 new PluginPageInfo
                 {
