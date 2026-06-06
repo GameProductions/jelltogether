482ef88a5a72d07fef1acc89942da50b9746c520
morenicano <831438+morenicano@users.noreply.github.com>
2026-06-05 23:22:17 -0400

    [v1.4.0] Publish patch release metadata

--- Web/jelltogether.js
+++ Web/jelltogether.js
@@ -8,7 +8,7 @@ class JellTogetherApp {
         this.enabledLibraryIds = [];
         this.allowQueueVotingByDefault = true;
         this.allowParticipantQueueAdds = true;
-        this.pluginVersion = "1.4.0.0";
+        this.pluginVersion = "1.4.0.1";
         this.changelog = [];
         this.currentRoom = null;
         this.currentUser = "Unknown";
